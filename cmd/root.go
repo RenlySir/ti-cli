85496509852cd48060b03cfe848eb2dfd2294da9
RenlySir <langongru610@163.com>
2022-01-12 23:40:10 +0800

    add get tidb info interface

--- cmd/root.go
+++ cmd/root.go
@@ -2,25 +2,53 @@ package cmd
 
 import (
 	"fmt"
-	"os"
+	"net"
+	"net/http"
 
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
+	"github.com/spf13/cobra/doc"
 )
 
 var (
-	// Used for flags.
-	cfgFile     string
-	userLicense string
-
-	rootCmd = &cobra.Command{
-		Use:   "cobra",
-		Short: "A generator for Cobra based Applications",
-		Long: `Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	}
+	host   net.IP
+	port   uint16
+	genDoc bool
+	pdHost net.IP
+	pdPort uint16
+	// ca        string
+	// sslCert   string
+	// sslKey    string
+	ctlClient *http.Client
+	// schema    string
+	user   string
+	passwd string
 )
+
+var rootCmd = &cobra.Command{
+	Use:   "ti-cli",
+	Short: "ti-cli controller",
+	Long:  `ti-cli is a command tool for tidb dba`,
+	RunE:  genDocument,
+}
+
+func genDocument(c *cobra.Command, args []string) error {
+	if !genDoc || len(args) != 0 {
+		return c.Usage()
+	}
+	docDir := "./doc"
+	docCmd := &cobra.Command{
+		Use:   "ti-cli",
+		Short: "ti-cli controller",
+		Long:  `ti-cli is a command tool for tidb dba`,
+	}
+	docCmd.AddCommand(getTiDBInfoCMD)
+	fmt.Println("Generating documents...")
+	if err := doc.GenMarkdownTree(docCmd, docDir); err != nil {
+		return err
+	}
+	fmt.Println("Done!")
+	return nil
+}
 
 // Execute executes the root command.
 func Execute() error {
@@ -28,39 +56,27 @@ func Execute() error {
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cobra.yaml)")
-	rootCmd.PersistentFlags().StringP("author", "a", "YOUR NAME", "author name for copyright attribution")
-	rootCmd.PersistentFlags().StringVarP(&userLicense, "license", "l", "", "name of license for the project")
-	rootCmd.PersistentFlags().Bool("viper", true, "use Viper for configuration")
-	viper.BindPFlag("author", rootCmd.PersistentFlags().Lookup("author"))
-	viper.BindPFlag("useViper", rootCmd.PersistentFlags().Lookup("viper"))
-	viper.SetDefault("author", "NAME HERE <EMAIL ADDRESS>")
-	viper.SetDefault("license", "apache")
+	rootCmd.AddCommand(getTiDBInfoCMD)
 
+	rootCmd.PersistentFlags().IPVarP(&host, "host", "", net.ParseIP("172.16.7.150"), "TiDB server port")
+	rootCmd.PersistentFlags().Uint16VarP(&port, "port", "", 10080, "TiDB server status port")
+	rootCmd.PersistentFlags().IPVarP(&pdHost, "pdHost", "", net.ParseIP("172.16.7.150"), "PD server port")
+	rootCmd.PersistentFlags().Uint16VarP(&pdPort, "pdPort", "", 2379, "PD server status port")
+	rootCmd.PersistentFlags().StringVarP(&user, "user", "", "", "user name")
+	rootCmd.PersistentFlags().StringVarP(&passwd, "password", "", "", "user passwd")
+	rootCmd.Flags().BoolVar(&genDoc, "gendoc", false, "generate doc file")
+	if err := rootCmd.Flags().MarkHidden("gendoc"); err != nil {
+		fmt.Printf("can not mark hidden flag, flag %s is not found", "gendoc")
+		return
+	}
 	// rootCmd.AddCommand(addCmd)
 	// rootCmd.AddCommand(initCmd)
-}
 
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := os.UserHomeDir()
-		cobra.CheckErr(err)
+	cobra.OnInitialize(func() {
+		ctlClient = &http.Client{
+			Transport: &http.Transport{},
+		}
+	})
 
-		// Search config in home directory with name ".cobra" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigType("yaml")
-		viper.SetConfigName(".cobra")
-	}
-
-	viper.AutomaticEnv()
-
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
-	}
 }
